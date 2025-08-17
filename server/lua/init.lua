f0874fcd7bccc1318a62b7e3a1ec173a764847eb
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-17 17:03:32 +0200

    feat: Add use of items

--- server/lua/init.lua
+++ server/lua/init.lua
@@ -6,6 +6,8 @@ local Server = require("selene.server")
 local Players = require("selene.players")
 local Entities = require("selene.entities")
 local Dimensions = require("selene.dimensions")
+local Network = require("selene.network")
+local Map = require("selene.map")
 
 Server.ServerStarted:Connect(function()
     print("Illarion Bridge started.")
@@ -44,5 +46,20 @@ Entities.SteppedOnTile:Connect(function(Entity, Coordinate)
     local warpAnnotation = Entity:CollisionMap(Coordinate):GetAnnotation(Coordinate, "illarion:warp")
     if warpAnnotation then
         Entity:SetCoordinate(warpAnnotation.ToX, warpAnnotation.ToY, warpAnnotation.ToLevel)
+    end
+end)
+
+Network.HandlePayload("illarion:use_at", function(Player, Payload)
+    local entity = Player:GetControlledEntity()
+    local dimension = entity.Dimension
+    local tiles = dimension:GetTilesAt(Payload.x, Payload.y, Payload.z, entity.Collision)
+    for _, tile in pairs(tiles) do
+        local tileScriptName = tile:GetMetadata("script")
+        if tileScriptName and tileScriptName ~= "\\N" then
+            local tileScript = require("illarion-vbu.server.lua." .. tileScriptName)
+            if type(tileScript.UseItem) == "function" then
+                tileScript.UseItem(Character.fromSelenePlayer(Player), Item.fromSeleneTile(tile))
+            end
+        end
     end
 end)
