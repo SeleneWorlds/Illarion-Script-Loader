9aaa0810f125cfd43953e1fe3b9ad7b44bb50772
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-19 08:30:16 +0200

    feat: Implement logger

---
+++ server/lua/logger.lua
@@ -0,0 +1,17 @@
+local Interface = require("illarion-api.server.lua.interface")
+
+Interface.Logger.Log = function(message)
+    print("[Log] " .. message)
+end
+
+Interface.Logger.LogDebug = function(message)
+    print("[Debug] " .. message)
+end
+
+Interface.Logger.LogError = function(message)
+    print("[Error] " .. message)
+end
+
+Interface.Logger.LogAdmin = function(message)
+    print("[Admin] " .. message)
+end
