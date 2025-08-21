7b7226c3067d57871e025cb585d0dacaa859d254
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-21 20:05:56 +0200

    feat: Add support for scheduled scripts

---
+++ server/lua/scheduled_scripts.lua
@@ -0,0 +1,23 @@
+local Registries = require("selene.registries")
+local Schedules = require("selene.schedules")
+
+local scheduledCallbacks = {}
+
+function scheduleNext(scheduledScript)
+    local interval = math.random(scheduledScript:GetField("minInterval"), scheduledScript:GetField("maxInterval"))
+    Schedules.SetTimeout(interval * 1000, scheduledCallbacks[scheduledScript.Name])
+end
+
+local allScheduledScripts = Registries.FindAll("illarion:scheduled_scripts")
+for _, scheduledScript in ipairs(allScheduledScripts) do
+    scheduledCallbacks[scheduledScript.Name] = function()
+       local scriptName = scheduledScript:GetField("script")
+       local functionName = scheduledScript:GetField("function")
+       local status, script = pcall(require, "illarion-vbu.server.lua." .. scriptName)
+       if status and type(script[functionName]) == "function" then
+           script[functionName]()
+           scheduleNext(scheduledScript)
+       end
+    end
+    scheduleNext(scheduledScript)
+end
