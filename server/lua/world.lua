9830665e34e025ad415ce91068cbf94377ecc45c
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-22 21:36:34 +0200

    fix: Add missing import

--- server/lua/world.lua
+++ server/lua/world.lua
@@ -1,11 +1,12 @@
 local Entities = require("selene.entities")
 local Registries = require("selene.registries")
+local Sounds = require("selene.sounds")
 local Interface = require("illarion-api.server.lua.interface")
 
-Interface.World.ShowGFX(gfxId, pos)
-    local entityType = Registries.FindByMetadata("entities", "gfxId", id)
+Interface.World.ShowGFX = function(gfxId, pos)
+    local entityType = Registries.FindByMetadata("entities", "gfxId", gfxId)
     if entityType == nil then
-        error("No entity for gfx id " .. id)
+        error("Unknown gfx id " .. gfxId)
     end
 
     local entity = Entities.CreateTransient(entityType.Name)
@@ -13,7 +14,7 @@ Interface.World.ShowGFX(gfxId, pos)
     entity:Spawn()
 end
 
-Interface.World.PlaySound(soundId, pos)
+Interface.World.PlaySound = function(soundId, pos)
     local sound = Registries.FindByMetadata("sounds", "soundId", soundId)
     if sound ~= nil then
         Sounds.PlaySoundAt(pos.x, pos.y, pos.z, sound.Name)
