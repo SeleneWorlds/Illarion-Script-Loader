a2eb52e3cccb06200523c207c2a478dcc17c1df1
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-22 21:35:20 +0200

    fix: Set ID and character type on NPCs

--- server/lua/npc.lua
+++ server/lua/npc.lua
@@ -8,5 +8,7 @@ for _, npc in ipairs(allNPCs) do
     entity.Name = npc:GetField("name")
     entity:SetCoordinate(npc:GetField("x"), npc:GetField("y"), npc:GetField("z"))
     entity:SetFacing(DirectionUtils.IllaToSelene(npc:GetField("facing")))
+    entity:SetCustomData(DataKeys.ID, npc:GetField("id") + 4278190080)
+    entity:SetCustomdata(DataKeys.CharacterType, Character.npc)
     entity:Spawn()
 end
