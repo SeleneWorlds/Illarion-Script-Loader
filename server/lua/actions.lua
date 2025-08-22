72f4011a1b99226df7dc6beaba6df303ce3421b5
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-22 21:36:53 +0200

    feat: Implement DisturbAction, SuccessAction, AbortAction, IsActionRunning

--- server/lua/actions.lua
+++ server/lua/actions.lua
@@ -2,6 +2,21 @@ local Schedules = require("selene.schedules")
 local Interface = require("illarion-api.server.lua.interface")
 local DataKeys = require("illarion-script-loader.server.lua.lib.datakeys")
 
+local function ClearAction(user)
+    local entity = user.SeleneEntity()
+    local action = entity:GetCustomData(DataKeys.CurrentAction, {})
+     if action.ActionHandle then
+         Schedules.ClearTimeout(action.ActionHandle)
+     end
+     if action.GfxHandle then
+         Schedules.ClearInterval(action.GfxHandle)
+     end
+     if action.SfxHandle then
+         Schedules.ClearInterval(action.SfxHandle)
+     end
+    entity:SetCustomData(DataKeys.CurrentAction, nil)
+end
+
 Interface.Actions.StartAction = function(user, duration, gfxId, gfxInterval, sfxId, sfxInterval)
     local entity = user.SeleneEntity()
     local gfxHandle = nil
@@ -18,15 +33,68 @@ Interface.Actions.StartAction = function(user, duration, gfxId, gfxInterval, sfx
     end
 
     local actionHandle = Schedules.SetTimeout(duration, function()
-        local script = entity:GetCustomData(DataKeys.LastActionScript)
+        local func = entity:GetCustomData(DataKeys.LastActionFunction)
         local args = entity:GetCustomData(DataKeys.LastActionArgs)
-        if type(script) == "function" and args then
-            script(table.unpack(args), Action.success)
+        if type(func) == "function" and args then
+            pcall(func, table.unpack(args), Action.success)
         end
+        ClearAction(user)
     end)
     entity:SetCustomData(DataKeys.CurrentAction, {
         ActionHandle = actionHandle,
         GfxHandle = gfxHandle,
         SfxHandle = sfxHandle
     })
+end
+
+Interface.Actions.DisturbAction = function(user, disturber)
+    -- TODO checkSource to invalidate target parameter if character logged out or monster died (castOnChar/useMonster)
+    -- TODO special handling for crafting dialogs
+    local shouldAbort = false
+    local entity = user.SeleneEntity()
+    local script = entity:GetCustomData(DataKeys.LastActionScript)
+    if script and type(script.actionDisturbed) == "function" then
+        shouldAbort = script.actionDisturbed(user, disturber)
+        entity:SetCustomData(DataKeys.CurrentAction, nil)
+    end
+
+    if shouldAbort then
+        Interface.Actions.AbortAction(user)
+        return true
+    end
+
+    return false
+end
+
+Interface.Actions.SuccessAction = function(user)
+    -- TODO checkSource to invalidate target parameter if character logged out or monster died (castOnChar/useMonster)
+    -- TODO special handling for crafting dialogs
+    local entity = user.SeleneEntity()
+    local func = entity:GetCustomData(DataKeys.LastActionFunction)
+     if type(func) == "function" then
+         pcall(func, table.unpack(args), Action.success)
+     end
+
+    ClearAction(user)
+end
+
+Interface.Actions.AbortAction = function(user)
+    -- TODO checkSource to invalidate target parameter if character logged out or monster died (castOnChar/useMonster)
+    -- TODO special handling for crafting dialogs
+    local entity = user.SeleneEntity()
+    local func = entity:GetCustomData(DataKeys.LastActionFunction)
+    if type(func) == "function" then
+        pcall(func, table.unpack(args), Action.abort)
+    end
+
+    ClearAction(user)
+end
+
+Interface.Actions.IsActionRunning = function(user)
+    local entity = user.SeleneEntity()
+    return entity:GetCustomData(DataKeys.CurrentAction) ~= nil
+end
+
+Interface.Actions.ChangeSource = function(user, charOrItemOrPosOrTextOrNil, talkTypeIfText)
+    print("ChangeSource", user) -- TODO
 end
