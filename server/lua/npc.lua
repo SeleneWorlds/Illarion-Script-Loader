d86eb2d4a8f123649fa1c98e29b63b1d46c62e04
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-23 10:48:29 +0200

    fix: Fix some derps in NPC loading

--- server/lua/npc.lua
+++ server/lua/npc.lua
@@ -1,6 +1,7 @@
 local Registries = require("selene.registries")
 local Entities = require("selene.entities")
 local DirectionUtils = require("illarion-api.server.lua.lib.directionUtils")
+local DataKeys = require("illarion-script-loader.server.lua.lib.datakeys")
 
 local allNPCs = Registries.FindAll("illarion:npcs")
 for _, npc in ipairs(allNPCs) do
@@ -8,7 +9,7 @@ for _, npc in ipairs(allNPCs) do
     entity.Name = npc:GetField("name")
     entity:SetCoordinate(npc:GetField("x"), npc:GetField("y"), npc:GetField("z"))
     entity:SetFacing(DirectionUtils.IllaToSelene(npc:GetField("facing")))
-    entity:SetCustomData(DataKeys.ID, npc:GetField("id") + 4278190080)
-    entity:SetCustomdata(DataKeys.CharacterType, Character.npc)
+    entity:SetCustomData(DataKeys.ID, npc:GetMetadata("id") + 4278190080)
+    entity:SetCustomData(DataKeys.CharacterType, Character.npc)
     entity:Spawn()
 end
