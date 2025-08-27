904f7dca17b9dab768ae90d23d944a7cfdb2f63f
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-27 13:26:42 +0200

    feat: Use attribute system for basic stats and belt inventory

--- server/lua/lifecycle.lua
+++ server/lua/lifecycle.lua
@@ -1,6 +1,7 @@
 local Server = require("selene.server")
 local Players = require("selene.players")
 local Entities = require("selene.entities")
+local Network = require("selene.network")
 local DataKeys = require("illarion-script-loader.server.lua.lib.datakeys")
 
 local illaReload = require("server.reload")
@@ -19,14 +20,42 @@ Players.PlayerJoined:Connect(function(player)
             }
         }
     end)
-    entity:SetCustomData(DataKeys.BaseAttributes .. "hitpoints", 10000)
-    entity:SetCustomData(DataKeys.BaseAttributes .. "foodlevel", 10000)
+    local healthAttribute = entity:GetOrCreateAttribute("hitpoints", 0)
+    healthAttribute:AddObserver(function(attribute, observer)
+        Network.SendToEntity(attribute.Owner, "illarion:health", { value = attribute.EffectiveValue / 10000 })
+    end)
+    local foodAttribute = entity:GetOrCreateAttribute("foodlevel", 0)
+    foodAttribute:AddObserver(function(attribute, observer)
+        Network.SendToEntity(attribute.Owner, "illarion:food", { value = attribute.EffectiveValue / 60000 })
+    end)
+    local manaAttribute = entity:GetOrCreateAttribute("mana", 0)
+    manaAttribute:AddObserver(function(attribute, observer)
+        Network.SendToEntity(attribute.Owner, "illarion:mana", { value = attribute.EffectiveValue / 10000 })
+    end)
+    local beltAttribute = entity:GetOrCreateAttribute("belt", tablex.managed({
+        slots = tablex.managed({
+            [12] = tablex.managed({}),
+            [13] = tablex.managed({}),
+            [14] = tablex.managed({}),
+            [15] = tablex.managed({}),
+            [16] = tablex.managed({}),
+            [17] = tablex.managed({}),
+        }),
+        slotIds = {12, 13, 14, 15, 16, 17}
+    }))
+    beltAttribute:AddObserver(function(attribute, observerData, observableData)
+        local item = attribute.Value:Lookup("slots", observableData, "item")
+        Network.SendToEntity(attribute.Owner, "illarion:update_slot", { viewId = observerData, slotId = observableData, item = item })
+    end, 1)
     entity:SetCustomData(DataKeys.ID, 8147)
     entity:SetCustomData(DataKeys.CharacterType, Character.player)
     entity:Spawn()
     player.ControlledEntity = entity
     player.CameraEntity = entity
     player:SetCameraToFollowTarget()
+
+    healthAttribute.Value = 10000
+    foodAttribute.Value = 60000
 
     player:SetCustomData(DataKeys.CurrentLoginTimestamp, os.time())
 
