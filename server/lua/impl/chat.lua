4a33bff0bc1e5006b889e77f4d07b85a72031c02
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-28 10:21:32 +0200

    feat: Add basic (global) chat

--- server/lua/impl/chat.lua
+++ server/lua/impl/chat.lua
@@ -1,8 +1,23 @@
+local Players = require("selene.players")
+local Network = require("selene.network")
+
 local DataKeys = require("illarion-script-loader.server.lua.lib.datakeys")
 
 Character.SeleneMethods.talk = function(user, mode, message, messageEnglish)
-    print("Talk", user.name, mode, message, messageEnglish)
     local entity = user.SeleneEntity
+    for _, player in ipairs(Players.GetOnlinePlayers()) do
+        -- TODO send to all in range
+        local effectiveMessage = message
+        if messageEnglish and user:getPlayerLanguage() == Player.english then
+            effectiveMessage = messageEnglish
+        end
+        Network.SendToPlayer(player, "illarion:chat", {
+            author = entity.NetworkId,
+            authorName = user.name,
+            mode = mode,
+            message = effectiveMessage
+        })
+    end
     entity.CustomData[DataKeys.LastSpokenText] = message
 end
 
@@ -29,3 +44,8 @@ world.broadcast = function(world, messageDe, messageEn)
         end
     end
 end
+
+Network.HandlePayload("illarion:chat", function(player, payload)
+    local character = Character.fromSelenePlayer(player)
+    character:talk(payload.mode, payload.message)
+end)
