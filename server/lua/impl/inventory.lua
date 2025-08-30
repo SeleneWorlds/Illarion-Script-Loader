97c07b6e7132f067c9210ad1e9f1bd5ef7499b3f
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-30 11:15:07 +0200

    feat: Implement some inventory functions, mostly still stubs though

--- server/lua/impl/inventory.lua
+++ server/lua/impl/inventory.lua
@@ -6,45 +6,141 @@ local DataKeys = require("illarion-script-loader.server.lua.lib.datakeys")
 
 Character.SeleneMethods.countItem = function(user, itemId)
     local count = 0
-    local items = user.SeleneEntity.CustomData[DataKeys.Inventory] or {}
-    for _, item in pairs(items) do
-        if item.id == itemId then
-            count = count + item.count
-        end
+    local itemDef = Registries.FindByMetadata("illarion:items", "id", itemId)
+    if not itemDef then
+        error("Tried to count unknown item id " .. itemId)
     end
-    local backpackItem = items[Character.backpack]
-    if backpackItem and backpackItem.container then
-        local backpack = Container.fromSeleneEntityData(user.SeleneEntity, backpackItem.container)
-        count = count + backpack:countItem()
+    local filter = function(item)
+        return item.def == itemDef
+    end
+    count = count + user.SeleneBelt:countItem(filter)
+    count = count + user.SeleneEquipment:countItem(filter)
+    count = count + user.SeleneBackpack:countItem(filter)
+    return count
+end
+
+Character.SeleneMethods.countItemAt = (user, where, itemId, data)
+    local count = 0
+    local itemDef = Registries.FindByMetadata("illarion:items", "id", itemId)
+    if not itemDef then
+        error("Tried to count unknown item id " .. itemId)
+    end
+    local filter = function(item)
+        return item.def == itemDef
+    end
+    if where == "all" or where == "belt" then
+        local belt = user.SeleneBelt
+        count = count + belt:countItem(filter)
+    end
+    if where == "all" or where == "body" then
+        local equipment = user.SeleneEquipment
+        count = count + equipment:countItem(filter)
+    end
+    if where == "all" or where == "backpack" then
+        local inventory = user.SeleneBackpack
+        count = count + inventory:countItem(filter)
     end
     return count
 end
 
 Character.SeleneMethods.getItemAt = function(user, slot)
-    local items = user.SeleneEntity.CustomData[DataKeys.Inventory] or {}
-    local item = items[slot]
-    if item then
-        return Item.fromSeleneEntityData(user.SeleneEntity, items, slot, item)
-    end
+    local inventory = user:GetSeleneInventoryByPos(pos)
+    local item = inventory:getItem("inventory:" .. pos)
+    -- TODO
     return Item.fromSeleneEmpty()
 end
 
+Character.SeleneMethods.changeQualityAt = function(user, pos, amount)
+    local inventory = user:GetSeleneInventoryByPos(pos)
+    if not inventory then
+        return
+    end
+
+    local item = inventory:getItem("inventory:" .. pos)
+    -- TODO changeQualityAt
+    print("changeQualityAt", tablex.tostring(item), amount)
+end
+
+Character.SeleneMethods.increaseAtPos = function(user, pos, amount)
+    local inventory = user:GetSeleneInventoryByPos(pos)
+    if not inventory then
+        return
+    end
+
+    return inventory:increaseItemAt("inventory:" .. pos, amount)
+end
+
 Character.SeleneMethods.createItem = function(user, itemId, count, quality, data)
-    local belt = user.SeleneBelt
     local itemDef = Registries.FindByMetadata("illarion:items", "id", itemId)
     if not itemDef then
         error("Tried to create unknown item id " .. itemId)
     end
-    belt:addItem({
-        name = itemDef.Name,
+    local rest = user.SeleneBelt:addItem({
+        def = itemDef,
         count = count,
         quality = quality,
         data = data
     })
+    if rest then
+        rest = user.SeleneBackpack:addItem({
+            def = itemDef,
+            count = count,
+            quality = quality,
+            data = data
+        })
+    end
+    return rest and rest.count or 0
+end
+
+Character.SeleneMethods.createAtPos = function(user, pos, itemId, count)
+    local itemDef = Registries.FindByMetadata("illarion:items", "id", itemId)
+    if not itemDef then
+        error("Tried to create unknown item id " .. itemId)
+    end
+    local inventory = user:GetSeleneInventoryByPos(pos)
+    -- TODO createAtPos
     return 0
 end
 
+Character.SeleneMethods.eraseItem = function(user, itemId, count, data)
+    local itemDef = Registries.FindByMetadata("illarion:items", "id", itemId)
+    if not itemDef then
+        error("Tried to erase unknown item id " .. itemId)
+    end
+    -- TODO eraseItem
+    return 0
+end
+
+Character.SeleneMethods.swapAtPos = function(user, pos, newId, newQuality)
+    local itemDef = Registries.FindByMetadata("illarion:items", "id", newId)
+    if not itemDef then
+        error("Tried to swap to unknown item id " .. newId)
+    end
+    -- TODO swapAtPos
+    return 0
+end
+
+Character.SeleneMethods.getItemList = function(user, itemId)
+    local itemDef = Registries.FindByMetadata("illarion:items", "id", itemId)
+    if not itemDef then
+        error("Tried to list unknown item id " .. itemId)
+    end
+    -- TODO getItemList
+    return 0
+end
+
+local equipmentSlotIds = { "inventory:0", "inventory:1", "inventory:2", "inventory:3", "inventory:4", "inventory:5", "inventory:6", "inventory:7", "inventory:8", "inventory:9", "inventory:10", "inventory:11" }
 local beltSlotIds = { "inventory:12", "inventory:13", "inventory:14", "inventory:15", "inventory:16", "inventory:17" }
+local backpackSlotIds = {}
+
+Character.SeleneMethods.GetSeleneInventoryByPos = function(user, pos)
+    if pos < 12 then
+        return user.SeleneEquipment
+    elseif pos < 18 then
+        return user.SeleneBelt
+    end
+    return nil
+end
 
 Character.SeleneMethods.GetSeleneInventory = function(user, inventoryName, slotIds)
     user.SeleneInventories = user.SeleneInventories or {}
@@ -58,4 +154,12 @@ end
 
 Character.SeleneGetters.SeleneBelt = function(user)
     return user:GetSeleneInventory("belt", beltSlotIds)
+end
+
+Character.SeleneGetters.SeleneEquipment = function(user)
+    return user:GetSeleneInventory("equipment", equipmentSlotIds)
+end
+
+Character.SeleneGetters.SeleneBackpack = function(user)
+    return user:GetSeleneInventory("backpack", backpackSlotIds)
 end
