030a52978adb63aad3ddd0b3530370f377b2b582
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-08-31 09:30:58 +0200

    feat: Implement id, pos, owner, itempos, inside, number, isLarge, getType, getData, setData for inventory and container items

--- server/lua/impl/item.lua
+++ server/lua/impl/item.lua
@@ -1,6 +1,8 @@
 Item.SeleneGetters.id = function(item)
     if item.SeleneTile then
         return tonumber(item.SeleneTile:GetMetadata("itemId"))
+    elseif item.SeleneItem then
+        return item.SeleneItem.def:GetMetadata("itemId")
     end
     return 0
 end
@@ -8,13 +10,38 @@ end
 Item.SeleneGetters.pos = function(item)
     if item.SeleneTile then
         return position.FromSeleneCoordinate(item.SeleneTile.Coordinate)
+    elseif item.owner then
+        return item.owner.pos
     end
     return position(0, 0, 0)
+end
+
+Item.SeleneGetters.owner = function(item)
+    if item.SeleneInventory then
+        return item.SeleneInventoryItem.owner
+    end
+    return nil
+end
+
+Item.SeleneGetters.itempos = function(item)
+    if item.SeleneInventoryItem then
+        return item.SeleneInventoryItem.slotId
+    end
+    return 0
+end
+
+Item.SeleneGetters.inside = function(item)
+    if item.SeleneInventory and item.SeleneInventory.isContainer then
+        return Container.fromSeleneInventory(item.SeleneInventory)
+    end
+    return nil
 end
 
 Item.SeleneGetters.number = function(item)
     if item.SeleneTile then
         return 1
+    elseif item.SeleneItem then
+        return item.SeleneItem.count
     end
     return 0
 end
@@ -23,6 +50,8 @@ Item.SeleneGetters.isLarge = function(item)
     if item.SeleneTile then
         local itemDef = Registries.FindByMetadata("illarion:items", "id", item.SeleneTile:GetMetadata("itemId"))
         return itemDef and itemDef:GetField("volume") >= 5000 or false
+    elseif item.SeleneItem then
+        return item.SeleneItem.def:GetField("volume") >= 5000 or false
     end
     return false
 end
@@ -30,6 +59,11 @@ end
 Item.SeleneMethods.getType = function(item)
     if item.SeleneTile then
         return scriptItem.field
+    elseif item.SeleneInventory and item.SeleneInventory.isContainer then
+        return scriptItem.container
+    elseif item.owner then
+        local slotId = item.itempos
+        return slotId < 12 and scriptItem.inventory or scriptItem.belt
     end
     return scriptItem.notdefined
 end
@@ -39,6 +73,8 @@ Item.SeleneMethods.getData = function(item, key)
         local dimension = item.SeleneTile.Dimension
         local data = dimension:GetAnnotationAt(item.SeleneTile.Coordinate, item.SeleneTile.Name)
         return data and data[key] or 0
+    elseif item.SeleneItem then
+        return item.SeleneItem.data[key] or 0
     end
     return 0
 end
@@ -49,11 +85,27 @@ Item.SeleneMethods.setData = function(item, key, value)
         local data = dimension:GetAnnotationAt(item.SeleneTile.Coordinate, item.SeleneTile.Name) or {}
         data[key] = value
         dimension:AnnotateTile(item.SeleneTile.Coordinate, item.SeleneTile.Name, data)
+    elseif item.SeleneItem then
+        item.SeleneItem.data[key] = value
     end
 end
 
-function Item.fromSeleneTile(Tile)
-   return setmetatable({SeleneTile = Tile}, Item.SeleneMetatable)
+function Item.fromSeleneInventoryItem(inventoryItem)
+    if inventoryItem == nil then
+        return Item.fromSeleneEmpty()
+    end
+    return setmetatable({
+        SeleneItem = inventoryItem.item,
+        SeleneInventory = inventoryItem.inventory
+        SeleneInventoryItem = inventoryItem
+    }, Item.SeleneMetatable)
+end
+
+function Item.fromSeleneTile(tile)
+    if tile == nil then
+        return Item.fromSeleneEmpty()
+    end
+   return setmetatable({SeleneTile = tile}, Item.SeleneMetatable)
 end
 
 function Item.fromSeleneEmpty()
