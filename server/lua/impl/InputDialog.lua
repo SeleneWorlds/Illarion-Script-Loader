4b8eafad3bbaf32bb0cf298a0f74ae2b23bb7e78
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-09-01 16:49:24 +0200

    feat: Add impls for input dialog

--- server/lua/impl/InputDialog.lua
+++ server/lua/impl/InputDialog.lua
@@ -8,3 +8,11 @@ InputDialog.SeleneConstructor = function(title, description, multiline, maxChars
         callback = callback
     }
 end
+
+InputDialog.SeleneMethods.getInput = function(self)
+    return self.input
+end
+
+InputDialog.SeleneMethods.getSuccess = function(self)
+    return self.success
+end
