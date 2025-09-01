b873017d7a87aae88d2522a13cbd8d381e872865
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-09-01 16:49:13 +0200

    feat: Add impls for crafting dialog

--- server/lua/impl/CraftingDialog.lua
+++ server/lua/impl/CraftingDialog.lua
@@ -1,9 +1,68 @@
+local Registries = require("selene.registries")
+
 CraftingDialog.SeleneConstructor = function(title, sfx, sfxDuration, callback)
     return {
         type = "CraftingDialog",
         title = title,
         sfx = sfx,
         sfxDuration = sfxDuration,
-        callback = callback
+        callback = callback,
+        groups = {},
+        craftables = {}
     }
+end
+
+CraftingDialog.SeleneMethods.clearGroupsAndProducts = function(self)
+    self.groups = {}
+    self.craftables = {}
+end
+
+CraftingDialog.SeleneMethods.addGroup = function(name)
+    table.insert(self.groups, name)
+end
+
+CraftingDialog.SeleneMethods.addCraftable = function(id, groupId, itemId, name, decisecondsToCraft, craftedStackSize)
+    local itemDef = Registries.FindByMetadata("illarion:items", "id", itemId)
+    if not itemDef then
+        error("Unknown item id " .. itemId)
+    end
+
+    local msToCraft = decisecondsToCraft * 100
+    self.craftables[id] = {
+        groupId = groupId,
+        itemDef = itemDef,
+        name = name,
+        duration = msToCraft,
+        craftedStackSize = craftedStackSize or 1,
+        ingredients = {}
+    }
+    self.lastCraftableId = id
+end
+
+CraftingDialog.SeleneMethods.addCraftableIngredient = function(itemId, number)
+    local itemDef = Registries.FindByMetadata("illarion:items", "id", itemId)
+    if not itemDef then
+        error("Unknown item id " .. itemId)
+    end
+
+    table.insert(self.craftables[self.lastCraftableId].ingredients, {
+        itemDef = itemDef,
+        count = number or 1
+    })
+end
+
+CraftingDialog.SeleneMethods.getResult = function()
+    return self.result
+end
+
+CraftingDialog.SeleneMethods.getCraftableId = function()
+    return self.craftableId
+end
+
+CraftingDialog.SeleneMethods.getCraftableAmount = function(self)
+    return self.craftableAmount
+end
+
+CraftingDialog.SeleneMethods.getIngredientIndex = function()
+    return self.ingredientIndex
 end
