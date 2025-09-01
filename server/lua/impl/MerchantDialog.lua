2430caa20082732ecd9f6ee02526f58eddc5ead5
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-09-01 16:59:04 +0200

    feat: Add impls for merchant and selection dialog

--- server/lua/impl/MerchantDialog.lua
+++ server/lua/impl/MerchantDialog.lua
@@ -1,7 +1,69 @@
+local Registries = require("selene.registries")
+
 MerchantDialog.SeleneConstructor = function(title, callback)
     return {
         type = "MerchantDialog",
         title = title,
-        callback = callback
+        callback = callback,
+        offers = {},
+        primaryRequests = {},
+        secondaryRequests = {}
     }
+end
+
+MerchantDialog.SeleneMethods.addOffer = function(self, itemId, name, price, buyStack)
+    local itemDef = Registries.FindByMetadata("illarion:items", "id", itemId)
+    if not itemDef then
+        error("Unknown item id " .. itemId)
+    end
+    table.insert(self.offers, {
+        itemDef = itemDef,
+        name = name,
+        price = price,
+        buyStack = itemDef:GetField("buyStack")
+    })
+end
+
+MerchantDialog.SeleneMethods.addPrimaryRequest = function(self, itemId, name, price)
+    local itemDef = Registries.FindByMetadata("illarion:items", "id", itemId)
+    if not itemDef then
+        error("Unknown item id " .. itemId)
+    end
+    table.insert(self.primaryRequests, {
+        itemDef = itemDef,
+        name = name,
+        price = price
+    })
+end
+
+MerchantDialog.SeleneMethods.addSecondaryRequest = function(self, itemId, name, price)
+    local itemDef = Registries.FindByMetadata("illarion:items", "id", itemId)
+    if not itemDef then
+        error("Unknown item id " .. itemId)
+    end
+    table.insert(self.secondaryRequests, {
+        itemDef = itemDef,
+        name = name,
+        price = price
+    })
+end
+
+MerchantDialog.SeleneMethods.getResult = function(self)
+    return self.result
+end
+
+MerchantDialog.SeleneMethods.getPurchaseIndex = function(self)
+    return self.purchaseIndex
+end
+
+MerchantDialog.SeleneMethods.getPurchaseAmount = function(self)
+    return self.purchaseAmount
+end
+
+MerchantDialog.SeleneMethods.getSaleItem = function(self)
+    return self.saleItem
+end
+
+MerchantDialog.SeleneMethods.getLookAtList = function(self)
+    return self.lookAtList
 end
--- server/lua/impl/SelectionDialog.lua
+++ server/lua/impl/SelectionDialog.lua
@@ -1,8 +1,27 @@
+local Registries = require("selene.registries")
+
 SelectionDialog.SeleneConstructor = function(title, callback)
     return {
         type = "SelectionDialog",
         title = title,
         message = message,
-        callback = callback
+        callback = callback,
+        options = {}
     }
+end
+
+SelectionDialog.SeleneMethods.addOption = function(self, id, name)
+    self.options[id] = name
+end
+
+SelectionDialog.SeleneMethods.setCloseOnMove = function(self)
+    self.closeOnMove = true
+end
+
+SelectionDialog.SeleneMethods.getSuccess = function(self)
+    return self.success
+end
+
+SelectionDialog.SeleneMethods.getSelectedIndex = function(self)
+    return self.selectedIndex
 end
