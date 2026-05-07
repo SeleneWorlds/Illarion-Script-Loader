a9db7ec78b94580c6caaf418733bbb26665dfa8f
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-05-07 17:21:19 +0200

    fix: Make server.learn optional, no Gobaith equivalent yet

--- server/lua/skills.lua
+++ server/lua/skills.lua
@@ -2,12 +2,14 @@ local Schedules = require("selene.schedules")
 
 local CharacterManager = require("illarion-script-loader.server.lua.lib.characterManager")
 
-local illaLearn = require("server.learn")
+local ok, illaLearn = pcall(require, "server.learn")
 
 Schedules.setInterval(10000, function()
     -- TODO I don't like that this holds a Lua-mem list of characters, it's brittle.
     --      Maybe we can have tag-based entity lookups?
     for _, character in pairs(CharacterManager.CharactersById) do
-        illaLearn.reduceMC(character)
+        if illaLearn then
+            illaLearn.reduceMC(character)
+        end
     end
 end)
