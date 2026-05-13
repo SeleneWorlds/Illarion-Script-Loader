057cf1667eac2b4199ebef832c40d5b7aef02b63
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-05-13 15:12:35 +0200

    fix: Field.tile is a getter, not a method

--- server/lua/impl/Field.lua
+++ server/lua/impl/Field.lua
@@ -1,6 +1,6 @@
 local InventoryManager = require("illarion-script-loader.server.lua.lib.inventoryManager")
 
-Field.SeleneMethods.tile = function(field)
+Field.SeleneGetters.tile = function(field)
     local tiles = field.SeleneDimension:getTilesAt(field.SelenePosition)
     if #tiles > 0 then
         return tiles[1]:getMetadata("tileId")
