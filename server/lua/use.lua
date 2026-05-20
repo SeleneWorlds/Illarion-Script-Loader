bb3bd476eed16945b490145f99fa2bdf23b7afda
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-05-20 10:46:44 +0200

    feat: Add support for using dropped items on map

--- server/lua/use.lua
+++ server/lua/use.lua
@@ -8,21 +8,43 @@ local InventoryManager = require("illarion-script-loader.server.lua.lib.inventor
 Network.handlePayload("illarion:use_at", function(player, payload)
     local playerEntity = player:getControlledEntity()
     local dimension = playerEntity:getDimension()
+    local illaUser = Character.fromSelenePlayer(player)
+    local illaPos = position(payload.x, payload.y, payload.z)
+    local actionData = playerEntity:getRuntimeData(DataKeys.LastAction)
 
     -- Entities can be either Monsters, NPCs, or non-static (dropped) items
     local entities = dimension:getEntitiesAt(payload.x, payload.y, payload.z, playerEntity:getCollisionViewer())
     for i = #entities, 1, -1 do
         local entity = entities[i]
+        if entity:hasTag("illarion:item") then
+
+            local itemId = entity:getEntityDefinition():getMetadata("itemId")
+            if itemId then
+                local item = Registries.findByMetadata("illarion:items", "id", itemId)
+                if item then
+                    local scriptName = item:getField("script")
+                    if scriptName then
+                        local status, script = pcall(require, scriptName)
+                        if status and type(script.UseItem) == "function" then
+                            local illaItem = Item.fromSeleneEntity(entity)
+                            actionData[DataFields.LastActionScript] = script
+                            actionData[DataFields.LastActionFunction] = script.UseItem
+                            actionData[DataFields.LastActionArgs] = { illaUser, illaItem }
+                            script.UseItem(illaUser, illaItem)
+                            return
+                        end
+                    end
+                end
+            end
+        end
+
         local charData = entity:getRuntimeData(DataKeys.Character)
-        local characterType = charData[DataFields.CharacterType]
+        local characterType = charData and charData[DataFields.CharacterType]
         if characterType == Character.monster then
             local scriptName = charData[DataFields.Script]
             if scriptName then
                 local status, script = pcall(require, scriptName)
                 if status and type(script.useMonster) == "function" then
-                    local illaUser = Character.fromSelenePlayer(player)
-                    local illaPos = position(payload.x, payload.y, payload.z)
-                    local actionData = playerEntity:getRuntimeData(DataKeys.LastAction)
                     actionData[DataFields.LastActionScript] = script
                     actionData[DataFields.LastActionFunction] = script.useMonster
                     actionData[DataFields.LastActionArgs] = { illaUser, illaPos }
@@ -36,8 +58,6 @@ Network.handlePayload("illarion:use_at", function(player, payload)
                 local status, script = pcall(require, scriptName)
                 if status and type(script.useNPC) == "function" then
                     local illaNpc = Character.fromSeleneEntity(entity)
-                    local illaUser = Character.fromSelenePlayer(player)
-                    local actionData = playerEntity:getRuntimeData(DataKeys.LastAction)
                     actionData[DataFields.LastActionScript] = script
                     actionData[DataFields.LastActionFunction] = script.useNPC
                     actionData[DataFields.LastActionArgs] = { illaNpc, illaUser }
@@ -57,9 +77,6 @@ Network.handlePayload("illarion:use_at", function(player, payload)
         if tileScriptName then
             local status, script = pcall(require, tileScriptName)
             if status and type(script.useTile) == "function" then
-                local illaUser = Character.fromSelenePlayer(player)
-                local illaPos = position(payload.x, payload.y, payload.z)
-                local actionData = playerEntity:getRuntimeData(DataKeys.LastAction)
                 actionData[DataFields.LastActionScript] = script
                 actionData[DataFields.LastActionFunction] = script.useTile
                 actionData[DataFields.LastActionArgs] = { illaUser, illaPos }
@@ -77,13 +94,12 @@ Network.handlePayload("illarion:use_at", function(player, payload)
                 if scriptName then
                     local status, script = pcall(require, scriptName)
                     if status and type(script.UseItem) == "function" then
-                        local illaUser = Character.fromSelenePlayer(player)
                         local illaItem = Item.fromSeleneTile(tile)
-                        local actionData = playerEntity:getRuntimeData(DataKeys.LastAction)
                         actionData[DataFields.LastActionScript] = script
                         actionData[DataFields.LastActionFunction] = script.UseItem
                         actionData[DataFields.LastActionArgs] = { illaUser, illaItem }
                         script.UseItem(illaUser, illaItem)
+                        return
                     end
                 end
             end
