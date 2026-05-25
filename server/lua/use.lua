c35cf1751ec9093ebd2b075a2bf0fde3d933decb
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-05-25 22:03:55 +0200

    fix: Fire npc use event even if no lua script is attached

--- server/lua/use.lua
+++ server/lua/use.lua
@@ -54,15 +54,16 @@ Network.handlePayload("illarion:use_at", function(player, payload)
                 end
             end
         elseif characterType == Character.npc then
+            local illaNpc = Character.fromSeleneEntity(entity)
+            Events.npcUsed:fire(illaNpc, illaUser)
+
             local scriptName = charData[DataFields.Script]
             if scriptName then
                 local status, script = pcall(require, scriptName)
                 if status and type(script.useNPC) == "function" then
-                    local illaNpc = Character.fromSeleneEntity(entity)
                     actionData[DataFields.LastActionScript] = script
                     actionData[DataFields.LastActionFunction] = script.useNPC
                     actionData[DataFields.LastActionArgs] = { illaNpc, illaUser }
-                    Events.npcUsed:fire(illaNpc, illaUser)
                     script.useNPC(illaNpc, illaUser)
                     return
                 end
